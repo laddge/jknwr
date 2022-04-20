52b055a13f1ee5193146a8c1aa57516b07fd822d
Laddge <dev.laddge@gmail.com>
2022-04-20 23:15:26 +0000

    [feat] レイアウト変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() {
-    runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
     const MyApp({Key? key}) : super(key: key);
-
     @override
-        Widget build(BuildContext context) {
-            return MaterialApp(
-                    title: 'Jknwr',
-                    home: Scaffold(
-                        appBar: AppBar(
-                            title: const Text('Jknwr'),
-                            ),
-                        body: const Center(
-                            child: Text('hello, world'),
-                            ),
-                        ),
-                    );
-        }
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            title: 'Jknwr',
+            home: Scaffold(
+                appBar: AppBar(
+                    title: const Text('Jknwr'),
+                    ),
+                body: const Text('hello, world'),
+                ),
+            );
+    }
 }
